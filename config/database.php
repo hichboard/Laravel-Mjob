0aabbb81861acb65528ce3c6cbe6c6ed11fdcb3f
hisham ih <hishaam.asn@gmail.com>
2018-06-08 17:20:03 +0000

    database setup

--- config/database.php
+++ config/database.php
@@ -1,5 +1,9 @@
 <?php
 
+
+$DATABASE_URL = parse_url(getenv("DATABASE_URL"));
+
+
 return [
 
     /*
@@ -13,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -54,7 +58,7 @@ return [
             'engine' => null,
         ],
 
-        'pgsql' => [
+      /*/  'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
@@ -65,6 +69,18 @@ return [
             'prefix' => '',
             'schema' => 'public',
             'sslmode' => 'prefer',
+        ],/*/
+         'pgsql' => [
+            'driver' => 'pgsql',
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
